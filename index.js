de140b4dc5a69680c496bcde2c5aa30fad931c8b
Samer Mobarak <samermobarak888@gmail.com>
2023-08-08 13:58:05 -0400

    functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+//test 1
+function saturdayFun(activity = `roller-skate`){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//test 2
+function mondayWork(activity = `go to the office`){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrap = "*") {
+  return function(adjective) {
+    return `You are ${wrap}${adjective}${wrap}!`;
+  };
+}
+
